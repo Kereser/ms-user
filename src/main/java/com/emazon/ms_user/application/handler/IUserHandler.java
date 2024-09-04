d2e235c5d5c90433889c1903bd9febaf8194e3c9
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-04 14:40:55 -0500

    Interfaces for handler, service and persistence port

--- src/main/java/com/emazon/ms_user/application/handler/IUserHandler.java
+++ src/main/java/com/emazon/ms_user/application/handler/IUserHandler.java
@@ -1,4 +1,8 @@
 package com.emazon.ms_user.application.handler;
 
+import com.emazon.ms_user.application.dto.RolesReq;
+import com.emazon.ms_user.application.dto.UserReqDTO;
+
 public interface IUserHandler {
+    void createUser(UserReqDTO dto, RolesReq type);
 }
---
+++ src/main/java/com/emazon/ms_user/domain/spi/IUserPersistencePort.java
@@ -0,0 +1,10 @@
+package com.emazon.ms_user.domain.spi;
+
+import com.emazon.ms_user.domain.model.User;
+
+import java.util.Optional;
+
+public interface IUserPersistencePort {
+    Optional<User> findByEmail(String name);
+    void createUser(User user);
+}
