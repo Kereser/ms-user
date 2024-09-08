6fc011489d5fbe2ca1dedda9610112642b57d562
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-08 12:40:20 -0500

    First functional security config

--- src/main/java/com/emazon/ms_user/infra/config/SecurityConfig.java
+++ src/main/java/com/emazon/ms_user/infra/config/SecurityConfig.java
@@ -1,62 +1,64 @@
 package com.emazon.ms_user.infra.config;
 
+import com.emazon.ms_user.infra.security.PasswordEncoderImpl;
+import com.emazon.ms_user.infra.security.entrypoint.CustomBasicAuthenticationEntryPoint;
+import com.emazon.ms_user.infra.security.entrypoint.CustomJWTEntryPoint;
+import com.emazon.ms_user.infra.security.filter.JwtValidatorFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/users/login").permitAll();
-                    auth.requestMatchers("/users").hasRole("ADMIN");
-                    auth.requestMatchers("/users/login-2").authenticated();
+    private final CustomBasicAuthenticationEntryPoint customBasicAuthenticationEntryPoint;
 
-                    auth.anyRequest().denyAll();
-                }).build();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CustomJWTEntryPoint jwtEntryPoint) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(customBasicAuthenticationEntryPoint))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers(HttpMethod.GET, "/users/login").permitAll();
+                auth.requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN");
+                auth.requestMatchers(HttpMethod.GET, "/users/login-2").hasRole("AUX_DEPOT");
+
+                auth.anyRequest().denyAll();
+            });
+
+        http.addFilterBefore(new JwtValidatorFilter(jwtEntryPoint), BasicAuthenticationFilter.class);
+
+        return http.build();
     }
 
     @Bean
     public AuthenticationManager authenticationManager(
-            UserDetailsService userDetailsService,
+            UserDetailsService userDetailsServiceImpl,
             PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setUserDetailsService(userDetailsServiceImpl);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
 
         return new ProviderManager(authenticationProvider);
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withUsername("user").password("1234").roles("ADMIN").build();
-
-        return new InMemoryUserDetailsManager(userDetails);
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new PasswordEncoderImpl().getEncoder();
     }
 }
