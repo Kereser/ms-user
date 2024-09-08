6717498eb98fd802029dc4560b47004ab4ce9260
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-08 16:33:28 -0500

    update interfaces and exceptions

--- src/main/java/com/emazon/ms_user/domain/model/IPasswordEncoder.java
+++ src/main/java/com/emazon/ms_user/domain/model/IPasswordEncoder.java
@@ -2,6 +2,4 @@ package com.emazon.ms_user.domain.model;
 
 public interface IPasswordEncoder {
     String encode(CharSequence rawPassword);
-
-    boolean matches(CharSequence rawPassword, String encodedPassword);
 }
--- src/main/java/com/emazon/ms_user/infra/exception/EmailAlreadyExists.java
+++ src/main/java/com/emazon/ms_user/infra/exception/EmailAlreadyExists.java
@@ -3,7 +3,9 @@ package com.emazon.ms_user.infra.exception;
 import com.emazon.ms_user.infra.out.jpa.entity.UserEntity;
 
 public class EmailAlreadyExists extends BaseEntityException {
+    private static final String EMAIL = "email";
+
     public EmailAlreadyExists() {
-        super(UserEntity.class.getSimpleName(), UserEntity.Fields.email);
+        super(UserEntity.class.getSimpleName(), EMAIL);
     }
 }
