9682a7401eaf23affc58fb0946f29422f79a9349
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-14 17:25:23 -0500

    Add new interface methods

--- src/main/java/com/emazon/ms_user/application/handler/IUserHandler.java
+++ src/main/java/com/emazon/ms_user/application/handler/IUserHandler.java
@@ -2,9 +2,9 @@ package com.emazon.ms_user.application.handler;
 
 import com.emazon.ms_user.application.dto.AuthResDTO;
 import com.emazon.ms_user.application.dto.UserReqDTO;
-import com.emazon.ms_user.domain.model.RoleEnum;
 
 public interface IUserHandler {
-    void createUser(UserReqDTO dto, RoleEnum role);
+    void createClientUser(UserReqDTO dto);
+    void createAuxDepotUser(UserReqDTO dto);
     AuthResDTO login();
 }
